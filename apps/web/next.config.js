bfde2e7cd15c17879240ed4a016f18bd87f55fe1
Juan Badano <jb@target-solutions.net>
2022-01-20 01:33:32 +0200

    C

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const withTM = require('next-transpile-modules')(['fade-on-screen'])
 
 const path = require('path')
 const Dotenv = require('dotenv-webpack')
@@ -7,27 +8,29 @@ const MomentLocalesPlugin = require('moment-locales-webpack-plugin')
 const withPWA =
   process.env.NODE_ENV != 'development' ? require('next-pwa') : (v) => v
 
-module.exports = {
-  webpack: (config) => {
-    config.plugins = config.plugins || []
+module.exports = withTM(
+  withPWA({
+    webpack: (config) => {
+      config.plugins = config.plugins || []
 
-    config.plugins = [
-      ...config.plugins,
-      new Dotenv({
-        path: path.join(__dirname, '.env'),
-        systemvars: true
-      }),
-      new MomentLocalesPlugin({
-        localesToKeep: ['en', 'es']
-      })
-    ]
+      config.plugins = [
+        ...config.plugins,
+        new Dotenv({
+          path: path.join(__dirname, '.env'),
+          systemvars: true
+        }),
+        new MomentLocalesPlugin({
+          localesToKeep: ['en', 'es']
+        })
+      ]
 
-    return config
-  },
-  pwa: {
-    dest: 'public'
-  },
-  publicRuntimeConfig: {
-    config: JSON.parse(process.env.config)
-  }
-}
+      return config
+    },
+    pwa: {
+      dest: 'public'
+    },
+    publicRuntimeConfig: {
+      config: JSON.parse(process.env.config)
+    }
+  })
+)
