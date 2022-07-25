03d8a61dab271369c7926dbc404563bd6e063667
Juan Badano <jb@target-solutions.net>
2022-07-25 13:19:38 +0300

    O

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -5,12 +5,4 @@ const withPWA =
   process.env.NODE_ENV != 'development' ? require('next-pwa') : (v) => v
 const { withConfig } = require('@jibadano/config/init')
 
-module.exports = withConfig(
-  withTM(
-    withPWA({
-      env: {
-        ASDASD: 'ASDADSSDASD'
-      }
-    })
-  )
-)
+module.exports = withConfig(withTM(withPWA({})))
