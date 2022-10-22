b25946c07ff0ea65dd66609eb34c88bef2687ecd
Juan Badano <jb@target-solutions.net>
2022-10-22 16:17:37 +0300

    D

--- preinstall.js
+++ preinstall.js
@@ -1,9 +1,10 @@
 require('dotenv').config({ path: './.env' })
 const { exec } = require('child_process')
 
+console.log(process.env.NODE_ENV, process.env.WORKSPACE)
 if (process.env.NODE_ENV != 'development') {
   if (process.env.WORKSPACE) {
-    if (process.env.WORKSPACE.startsWith('services')) {
+    if (process.env.WORKSPACE.indexOf('services') > -1) {
       console.log('removing packages and apps')
       exec('rm -rf apps packages', (err) => {
         process.exit(err ? 1 : 0)
