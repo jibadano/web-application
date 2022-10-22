5c3d22769ef640f37726f7d99a6caba3e81c1f61
Juan Badano <jb@target-solutions.net>
2022-10-22 15:56:54 +0300

    A

---
+++ preinstall.js
@@ -0,0 +1,16 @@
+require('dotenv').config({ path: './.env' })
+const { exec } = require('child_process')
+
+if (process.env.NODE_ENV == 'production') {
+  if (process.env.WORKSPACE) {
+    if (process.env.WORKSPACE.startsWith('services')) {
+      exec('rm -rf apps packages', (err) => {
+        process.exit(err ? 1 : 0)
+      })
+    }
+  } else {
+    exec('rm -rf apps packages services', (err) => {
+      process.exit(err ? 1 : 0)
+    })
+  }
+}
