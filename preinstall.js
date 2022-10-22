beb2796b7f848302af8f4e04ff16dcf896999220
Juan Badano <jb@target-solutions.net>
2022-10-22 16:14:49 +0300

    C

--- preinstall.js
+++ preinstall.js
@@ -4,11 +4,14 @@ const { exec } = require('child_process')
 if (process.env.NODE_ENV != 'development') {
   if (process.env.WORKSPACE) {
     if (process.env.WORKSPACE.startsWith('services')) {
+      console.log('removing packages and apps')
       exec('rm -rf apps packages', (err) => {
         process.exit(err ? 1 : 0)
       })
     }
   } else {
+    console.log('removing all')
+
     exec('rm -rf apps packages services', (err) => {
       process.exit(err ? 1 : 0)
     })
